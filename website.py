60be29f28a5b20fe4d47488fb015e3662282af3a
Shanmukha Appajosyula <149186397+Shanmukha2005@users.noreply.github.com>
2025-03-01 22:22:50 +0530

    Create website.py

---
+++ website.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+# Open and read the HTML file
+with open("mywebsite.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+# Render the HTML/JS website
+st.title("")
+html(html_content, height="None")
