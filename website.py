eaeb0af50747548380a827526640dee66cf91a90
Shanmukha Appajosyula <149186397+Shanmukha2005@users.noreply.github.com>
2025-03-01 22:42:12 +0530

    Update website.py

--- website.py
+++ website.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit.components.v1 import html
 
 # Open and read the HTML file
-with open("mywebsite.html", "r", encoding="utf-8") as f:
+with open("website.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
 # Render the HTML/JS website
